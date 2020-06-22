d38516773edf519783fab9118fdfc82226b5001a
BrandonDalbec <32801607+BrandonDalbec@users.noreply.github.com>
2020-06-22 02:06:24 -0700

    more tune ups

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.blog_index_view),
     path('create_blog/', views.create_blog_view),
-    path('published_blogs/', views.published_blogs_view)
+    path('published_blogs/', views.published_blogs_view),
+    path(r'/individual_blog/(?P<id>\d+)/$', views.individual_blog_view),
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.template import loader
 
 from .models import Article
 
@@ -27,4 +28,11 @@ def create_blog_view(request):
 
 def published_blogs_view(request):
     articles = Article.objects.all()
-    return render(request, 'blog/publishedBlogs.html', context={'articles': articles})
+    template = loader.get_template('blog/publishedBlogs.html')
+    context={'articles': articles}
+    return HttpResponse(template.render(context, request))
+
+def individual_blog_view(request, id):
+    article = Article.objects.get(pk=id)
+    articleContent = article.content
+    return render(request, 'blog/individualBlog.html', context={'article': article, 'articleContent': articleContent})
